c311d3c1686f308128e32cf971801ced8ddd2ae5
ihagoSantos <ihagog12@gmail.com>
2025-02-18 23:16:29 -0300

    feat: hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,35 @@
-import styles from "./Navbar.module.css"
+"use client"
+
+import "./Navbar.css"
+import { useState } from "react"
 
 export const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <nav className={styles.navbar}>
-            <ul>
+        <nav className="navbar">
+            {/* Botão Hambúrguer */}
+            <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? "✖" : "☰"}
+            </button>
+
+            <ul className="menu">
                 <li><a href="#">INÍCIO</a></li>
                 <li><a href="">SOBRE MIM</a></li>
                 <li><a href="">SERVIÇOS</a></li>
                 <li><a href="">DEPOIMENTOS</a></li>
                 <li><a href="">CONTATO</a></li>
             </ul>
+            
+            <ul className={`menu-mobile ${isOpen ? "open" : ""}`}>
+                <li><a href="#">INÍCIO</a></li>
+                <li><a href="">SOBRE MIM</a></li>
+                <li><a href="">SERVIÇOS</a></li>
+                <li><a href="">DEPOIMENTOS</a></li>
+                <li><a href="">CONTATO</a></li>
+            </ul>
+            
+            
         </nav>
     )
 }
